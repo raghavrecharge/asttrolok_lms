de0b77026e4c02d84af4c291be54043b75f42026
lmsasttrolok <server@rechargestudio.com>
2025-12-04 13:56:15 +0000

    fixed installment bugs

--- resources/views/web/default2/panel/financial/installments/details.blade.php
+++ resources/views/web/default2/panel/financial/installments/details.blade.php
@@ -21,7 +21,7 @@
             <div class="row">
                 <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center">
                     <div class="d-flex flex-column align-items-center text-center">
-                        <img src="{{ config('app.js_css_url') }}/assets/default/img/activity/127.png" width="64" height="64" alt="">
+                        <img loading="lazy"  src="{{ config('app.js_css_url') }}/assets/default/img/activity/127.png" width="64" height="64" alt="">
                         <strong class="font-30 text-dark-blue font-weight-bold mt-5">{{ $totalParts }}</strong>
                         <span class="font-16 text-gray font-weight-500">{{ trans('update.total_parts') }}</span>
                     </div>
@@ -29,7 +29,7 @@
 
                 <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center">
                     <div class="d-flex flex-column align-items-center text-center">
-                        <img src="{{ config('app.js_css_url') }}/assets/default/img/activity/38.svg" width="64" height="64" alt="">
+                        <img loading="lazy"  src="{{ config('app.js_css_url') }}/assets/default/img/activity/38.svg" width="64" height="64" alt="">
                         <strong class="font-30 text-dark-blue font-weight-bold mt-5">{{ $remainedParts }}</strong>
                         <span class="font-16 text-gray font-weight-500">{{ trans('update.remained_parts') }}</span>
                     </div>
@@ -37,7 +37,7 @@
 
                 <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center mt-5 mt-md-0">
                     <div class="d-flex flex-column align-items-center text-center">
-                        <img src="{{ config('app.js_css_url') }}/assets/default/img/activity/33.png" width="64" height="64" alt="">
+                        <img loading="lazy"  src="{{ config('app.js_css_url') }}/assets/default/img/activity/33.png" width="64" height="64" alt="">
                         <strong class="font-30 text-dark-blue font-weight-bold mt-5">{{ handlePrice($remainedAmount) }}</strong>
                         <span class="font-16 text-gray font-weight-500">{{ trans('update.remained_amount') }}</span>
                     </div>
@@ -45,7 +45,7 @@
 
                 <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center mt-5 mt-md-0">
                     <div class="d-flex flex-column align-items-center text-center">
-                        <img src="{{ config('app.js_css_url') }}/assets/default/img/activity/128.png" width="64" height="64" alt="">
+                        <img loading="lazy"  src="{{ config('app.js_css_url') }}/assets/default/img/activity/128.png" width="64" height="64" alt="">
                         <strong class="font-30 text-dark-blue font-weight-bold mt-5">{{ handlePrice($overdueAmount) }}</strong>
                         <span class="font-16 text-gray font-weight-500">{{ trans('update.overdue_amount') }}</span>
                     </div>
@@ -75,6 +75,10 @@
                             </tr>
                             </thead>
                             <tbody>
+                                @php
+                                            $count = 0;
+                                            $count11 = 0;
+                                            @endphp
 
                             @if(!empty($installment->upfront))
                                 @php
@@ -88,20 +92,53 @@
                                         @endif
                                     </td>
 
-                                    <td class="text-center">{{ handlePrice($installment->getUpfront($itemPrice)) }}</td>
+                                    <td class="text-center">
+
+                                    @if($upfrontPayment->status == "paid")
+                                            {{ handlePrice($installment->getUpfront($itemPrice)) }}
+                                            @php
+                                            $paidAmount = $paidAmount - $installment->getUpfront($itemPrice);
+                                            @endphp
+                                        @else
+                                            {{ handlePrice($installment->getUpfront($itemPrice)) }} - {{ $paidAmount }}
+                                            @php
+                                            $count11 = 1;
+                                            @endphp
+                                        @endif
+
+                                        </td>
 
                                     <td class="text-center">-</td>
 
                                     <td class="text-center">{{ !empty($upfrontPayment) ? dateTimeFormat($upfrontPayment->created_at, 'j M Y H:i') : '-' }}</td>
 
                                     <td class="text-center">
-                                        @if(!empty($upfrontPayment))
+                                        @if($upfrontPayment->status == "paid")
                                             <span class="text-primary">{{ trans('public.paid') }}</span>
+                                            @php
+                                             $paidAmount;
+                                            @endphp
                                         @else
                                             <span class="text-dark-blue">{{ trans('update.unpaid') }}</span>
+                                            @if($count==0) @php $count=1; @endphp@endif
                                         @endif
                                     </td>
                                     <td class="text-right">
+                                        @if($count11==1)
+                                        <div class="btn-group dropdown table-actions">
+                                                <button type="button" class="btn-transparent dropdown-toggle"
+                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i data-feather="more-vertical" height="20"></i>
+                                                </button>
+
+                                                <div class="dropdown-menu menu-lg">
+
+                                                    <a href="{{ config('app.manual_base_url')}}/register-course/{{$webinar_title}}" target="_blank"
+                                                       class="webinar-actions d-block mt-10 font-weight-normal">Pay Upcomming Part</a>
+                                                </div>
+                                            </div>
+                                        @endif
 
                                     </td>
                                 </tr>
@@ -126,7 +163,23 @@
                                         <span class="d-block font-12 text-gray">{{ trans('update.n_days_after_purchase', ['days' => $step->deadline]) }}</span>
                                     </td>
 
-                                    <td class="text-center">{{ handlePrice($step->getPrice($itemPrice)) }}</td>
+                                    <td class="text-center">
+
+                                    @if(!empty($stepPayment))
+                                            {{ handlePrice($step->getPrice($itemPrice)) }}
+                                            @php
+                                            $paidAmount = $paidAmount - $step->getPrice($itemPrice);
+                                            @endphp
+
+                                        @else
+                                            @if($paidAmount==0)
+                                            {{ handlePrice($step->getPrice($itemPrice)) }} @if($count==0) @php $count=1; @endphp@endif
+                                            @else
+                                            {{ handlePrice($step->getPrice($itemPrice)) }} @if($count==0)<span class="text-primary">- {{ handlePrice($paidAmount) }}</span> @php $count=1; @endphp@endif
+                                            @endif
+                                        @endif
+
+                                    </td>
 
                                     <td class="text-center">
                                         <span class="{{ $isOverdue ? 'text-danger' : '' }}">{{ dateTimeFormat($dueAt, 'j M Y') }}</span>
@@ -137,11 +190,15 @@
                                     <td class="text-center">
                                         @if(!empty($stepPayment))
                                             <span class="text-primary">{{ trans('public.paid') }}</span>
+                                            @php
+                                             $paidAmount;
+                                            @endphp
                                         @else
                                             <span class="{{ $isOverdue ? 'text-danger' : 'text-dark-blue' }}">{{ trans('update.unpaid') }} {{ $isOverdue ? "(". trans('update.overdue') .")" : '' }}</span>
                                         @endif
                                     </td>
                                     <td class="text-right">
+                                        @if($count11==0)
                                         @if(empty($stepPayment))
                                             <div class="btn-group dropdown table-actions">
                                                 <button type="button" class="btn-transparent dropdown-toggle"
@@ -149,12 +206,14 @@
                                                         aria-expanded="false">
                                                     <i data-feather="more-vertical" height="20"></i>
                                                 </button>
+
                                                 <div class="dropdown-menu menu-lg">
 
-                                                    <a href="/panel/financial/installments/{{ $order->id }}/steps/{{ $step->id }}/pay" target="_blank"
-                                                       class="webinar-actions d-block mt-10 font-weight-normal">{{ trans('panel.pay') }}</a>
+                                                    <a href="/panel/financial/installments/{{ $order->id }}/pay_upcoming_part" target="_blank"
+                                                       class="webinar-actions d-block mt-10 font-weight-normal">Pay Upcomming Part</a>
                                                 </div>
                                             </div>
+                                        @endif
                                         @endif
                                     </td>
                                 </tr>
